01fc6a65faf40a9d9017cc72613f9443a10256d3
Douglas Berkley <douglasmberkley@gmail.com>
2017-09-29 10:30:47 +0900

    sortin by name on group index

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,7 +2,7 @@ class GroupsController < ApplicationController
   def index
     @tournament = Tournament.find(params[:tournament_id])
     @groups = @tournament.groups
-    @players = Player.all
+    @players = Player.all.order("name ASC")
     @teams = Team.all.order("rating DESC")
   end
 end
